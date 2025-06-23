48882c894da5871d8294e74d26f98427d7b84d7f
Zaibunis <165600878+Zaibunis@users.noreply.github.com>
2025-06-24 01:57:10 +0500

    new-changes

--- src/app/component/thanks/page.tsx
+++ src/app/component/thanks/page.tsx
@@ -4,7 +4,7 @@ export default function ThankYouPage() {
       <div>
         <h1 className="text-4xl font-bold mb-4">🎉 Thank You!</h1>
         <p className="text-lg">Your message has been sent successfully.</p>
-        <p className="mt-2">I'll get back to you as soon as possible.</p>
+        <p className="mt-2">I&apos;ll get back to you as soon as possible.</p>
         <a href="/" className="mt-6 inline-block px-6 py-3 bg-white text-purple-600 rounded-lg shadow hover:bg-gray-100 transition">
           Back to Home
         </a>
